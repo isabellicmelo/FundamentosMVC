4f4028b0d4abfdc4cdd67966c5b9a71b40b4d259
bonaldisza <blackgamerjh@gmail.com>
2025-05-29 08:19:11 -0300

    Subindo o FundamentosMVC dia 29/05

---
+++ FundamentosMVC/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using System.Diagnostics;
+using FundamentosMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FundamentosMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost] 
+        public ActionResult Lista (Pessoa pessoa)
+        {
+            return View (pessoa);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FundamentosMVC/Models/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FundamentosMVC.Models
+{
+    public class Pessoa
+    {
+        [Display(Name = "Código do CLiente")]
+
+        public int ? PessoaId { get; set; }
+
+        [Display(Name = "Nome do Cliente")]
+
+        public string ? Nome { get; set; }
+        public string ? Email { get; set; }
+    }
+}
